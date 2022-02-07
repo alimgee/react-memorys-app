05ce30aed1e16fcc362d34324573315ca48808f0
alimgee <mcgee.al@gmail.com>
2022-02-07 19:57:23 +0000

    Setting up testing

---
+++ src/mocks/handlers.js
@@ -0,0 +1,49 @@
+import { rest } from "msw";
+
+const baseURL = "https://drf-api-rec.herokuapp.com/";
+
+export const handlers = [
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        "id": 1,
+        "owner": "testal",
+        "created_at": "09 Nov 2021",
+        "updated_at": "09 Nov 2021",
+        "name": "",
+        "content": "",
+        "image": "https://res.cloudinary.com/dzh7nbsay/image/upload/v1/media/../default_profile_qdjgyp",
+        "is_owner": false,
+        "following_id": null,
+        "posts_count": 2,
+        "followers_count": 0,
+        "following_count": 1
+      })
+    );
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];import { rest } from "msw";
+
+const baseURL = "https://ci-drf-api.herokuapp.com/";
+
+export const handlers = [
+  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+    return res(
+      ctx.json({
+        pk: 2,
+        username: "brian",
+        email: "",
+        first_name: "",
+        last_name: "",
+        profile_id: 2,
+        profile_image:
+          "https://res.cloudinary.com/dgjrrvdbl/image/upload/v1/media/../default_profile_qdjgyp",
+      })
+    );
+  }),
+  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+    return res(ctx.status(200));
+  }),
+];
