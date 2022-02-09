5e1f1438bbf1536ab675475ee223486b79b37881
alimgee <mcgee.al@gmail.com>
2022-02-09 12:53:26 +0000

    Adding tests for navbar component

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,7 +1,6 @@
 import { rest } from "msw";
 
-const baseURL = "https://drf-api-rec.herokuapp.com/";
-
+const baseURL = "https://ci-drf-api.herokuapp.com/";
 export const handlers = [
   rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
     return res(
@@ -24,26 +23,6 @@ export const handlers = [
   rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
     return res(ctx.status(200));
   }),
-];import { rest } from "msw";
-
-const baseURL = "https://ci-drf-api.herokuapp.com/";
-
-export const handlers = [
-  rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
-    return res(
-      ctx.json({
-        pk: 2,
-        username: "brian",
-        email: "",
-        first_name: "",
-        last_name: "",
-        profile_id: 2,
-        profile_image:
-          "https://res.cloudinary.com/dgjrrvdbl/image/upload/v1/media/../default_profile_qdjgyp",
-      })
-    );
-  }),
-  rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
-    return res(ctx.status(200));
-  }),
 ];
+
+
