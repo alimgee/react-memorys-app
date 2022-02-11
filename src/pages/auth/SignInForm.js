911a305181168797d54f17a08e5744044544cd4c
alimgee <mcgee.al@gmail.com>
2022-02-11 13:54:54 +0000

    setting and getting refresh tokens in local sotrage

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -17,6 +17,7 @@ import appStyles from "../../App.module.css";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 
 import { useRedirect } from "../../hooks/useRedirect";
+import { setTokenTimeStamp} from '../../utils/utils';
 
 function SignInForm() {
   const setCurrentUser = useSetCurrentUser();
@@ -37,6 +38,7 @@ function SignInForm() {
     try {
       const { data } = await axios.post("/dj-rest-auth/login/", signInData);
       setCurrentUser(data.user);
+      setTokenTimeStamp(data);
       history.push("/");
     } catch (err) {
       setErrors(err.response?.data);
