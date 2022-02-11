1dec45b44b4eccd1e2f015527b06c7726b672444
alimgee <mcgee.al@gmail.com>
2022-02-11 14:02:42 +0000

    Adding remove token timestamp calls in contexts and nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,6 +10,7 @@ import {
 import Avatar from "./Avatar";
 import axios from "axios";
 import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
+import { removeTokenTimeStamp } from "../utils/utils";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
@@ -21,6 +22,7 @@ const NavBar = () => {
     try {
       await axios.post("dj-rest-auth/logout/");
       setCurrentUser(null);
+      removeTokenTimeStamp();
     } catch (err) {
       console.log(err);
     }
